ba38e3fe665e66a0bd531997e6ea1d73fb7abb27
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-05 19:31:41 +0300

    Removed unused styling

--- src/App.vue
+++ src/App.vue
@@ -22,21 +22,9 @@ export default {
 </script>
 
 <style>
-body {
-  margin: 0;
-  padding: 0;
-  min-height: 100%;
-  width: 100%;
-}
 .container {
   background-color: #eeeeee;
   display: inline;
-  width: 100%;
-}
-html {
-  margin: 0;
-  padding: 0;
-  height: 100%;
   width: 100%;
 }
 </style>
