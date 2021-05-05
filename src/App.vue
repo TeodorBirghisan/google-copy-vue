55dd1354f8aad9a227c199d1b1a7843371e98ed1
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-05 19:31:13 +0300

    Added Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
-   <html>
-    <body>
-      <div class="container">
-        <Header />
-        <Search />
-      </div>
-    </body>
-  </html>
+    <div class="container">
+      <Header />
+      <Search />
+      <Footer />
+    </div>
 </template>
 
 <script>
 import Header from '../src/components/Header';
 import Search from '../src/components/Search';
+import Footer from '../src/components/Footer';
 
 export default {
   name: 'App',
   components: {
     Header,
-    Search
+    Search,
+    Footer,
   }
 }
 </script>
